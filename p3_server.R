898563ba56130982ced5d82e982d0323f75faaf2
shihanyin <shihay3@uw.edu>
2022-03-09 14:45:32 -0800

    plot1 finished

--- p3_server.R
+++ p3_server.R
@@ -2,8 +2,19 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 
+county_cases <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+wa <- county_cases %>% filter(state == "Washington")
 
 server <- function(input, output) {
   
- 
+  output$Plot1 <- renderPlotly({
+    
+    wa <- wa %>%  filter(county %in% input$user_category)
+    
+    Plot1 <- ggplot(data = wa) + geom_line(mapping = aes(x = date, y = cases, color= county))
+    
+    plotly_Plot1 <- ggplotly(Plot1)
+    
+    return(plotly_Plot1)
+  })
 }
--- p3_ui.R
+++ p3_ui.R
@@ -2,13 +2,34 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
-#I think this should be the basic structure of this project.
+
+plot_sidebar1 <- sidebarPanel(
+  selectInput(
+    inputId = "user_category",
+    label = h4("Select County"),
+    choices = wa$county,
+    selected = "King",
+    multiple = TRUE)
+  
+)
+
+
+plot_main <- mainPanel(
+  plotlyOutput(outputId = "Plot1" ),
+  p("By looking at this chart, we can get a glance about trend of covid cases among counties.")
+)
+  
+
+plot_1 <- tabPanel(
+  "Plot 1",
+  sidebarLayout(
+    plot_sidebar1,
+    plot_main
+  )
+)
+
 
 ui <- navbarPage(
   "P03",
-  intro_tab,
-  plot_1,
-  plot_2,
-  plot_3,
-  sum_tab
+  plot_1
 )
