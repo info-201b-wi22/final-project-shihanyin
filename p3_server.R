6ff3cd38c90eea695896a2541ecbc74f1e3f34b1
megcheu <megancheung@MacBook-Pro-4.local>
2022-03-10 01:57:39 -0800

    completed plot 2 and troubleshooting

--- p3_server.R
+++ p3_server.R
@@ -4,11 +4,11 @@ library(dplyr)
 library(stringr)
 library(readr)
 
-county_cases <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+county_cases <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 wa <- county_cases %>% filter(state == "Washington")
 
 #plot2
-criminal_data_type<- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/criminal_data_type.csv?token=GHSAT0AAAAAABQW2MUKKXLPSEX4VDP3TLRMYRSWGNQ")
+criminal_data_type<- read_csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/criminal_data_type.csv?token=GHSAT0AAAAAABQW2MUKKXLPSEX4VDP3TLRMYRSWGNQ")
 personal_crime <- criminal_data_type[c(1:14, 37:50), ]
 property_crime <- criminal_data_type[c(16:27, 52:63), ]
 society_crime <- criminal_data_type[c(29:35, 65:71), ]
@@ -25,22 +25,25 @@ server <- function(input, output) {
     
     return(plotly_Plot1)
   })
-  
-  output$Plot2 <- renderPlotly({ input$radio 
-    if (input$plot_type == "Personal Crime") {
+
+  output$Plot2 <- renderPlotly({ 
+    if (input$plot_type == 1) {
       ggplot(personal_crime) + 
-        geom_col(aes(x=year, y=Incidents, fill = Offense)) +
-        scale_fill_brewer(palette = 3)
-    } else if (input$plot_type == "Property Crime") {
+        geom_col(aes(x=year, y=Incidents, fill = Offense))+
+        labs(title = "Personal Crime Incidences", subtitle = "2019-2020",x = "Year", y = "Incidents") 
+    } else if (input$plot_type == 2) {
       ggplot(property_crime) +
-        geom_col(aes(x=year, y=Incidents, fill = Offense))
+        geom_col(aes(x=year, y=Incidents, fill = Offense))+
+        labs(title = "Property Crime Incidences", subtitle = "2019-2020",x = "Year", y = "Incidents") 
     }
-    else if (input$plot_type == "Society Crime") {
+    else if (input$plot_type == 3) {
       ggplot(society_crime) +
         geom_col(aes(x=year, y=Incidents, fill = Offense)) +
-        scale_fill_brewer(palette = 2)
+        scale_fill_brewer(palette = 2)+
+        labs(title = "Society Crime Incidences", subtitle = "2019-2020",x = "Year", y = "Incidents") 
     }
-    return(plotly_Plot2)
   })
 }
+
+
 
--- p3_ui.R
+++ p3_ui.R
@@ -3,6 +3,7 @@ library(plotly)
 library(bslib)
 
 
+
 plot_sidebar1 <- sidebarPanel(
   selectInput(
     inputId = "user_category",
@@ -13,22 +14,10 @@ plot_sidebar1 <- sidebarPanel(
   
 )
 
-plot_sidebar2 <- sidebarPanel(
-  radioButtons(
-    inputId = "plot_type",
-    label = h3("Different Distribution of Crimes"),
-    choices = list("Personal Crime" = 1, "Property Crime" = 2, "Society Crime" = 3), 
-    selected = 1),
-  
-  plotOutput("Plot2")
-  )
-  
 plot_main <- mainPanel(
   plotlyOutput(outputId = "Plot1" ),
-  plotlyOutput(outputId = "Plot2" ),
   p("By looking at this chart, we can get a glance about trend of covid cases among counties.")
 )
-  
 
 plot_1 <- tabPanel(
   "Plot 1",
@@ -38,11 +27,21 @@ plot_1 <- tabPanel(
   )
 )
 
+
+
 plot_2 <- tabPanel(
   "Plot 2", 
   sidebarLayout(
-    plot_sidebar2,
-    plot_main
+    sidebarPanel(
+      radioButtons(
+        inputId = "plot_type",
+        label = h3("Different Distribution of Crimes"),
+        choices = list("Personal Crime" = 1, "Property Crime" = 2, "Society Crime" = 3), 
+        selected = 1),
+      ),
+    mainPanel(
+      plotlyOutput("Plot2" )
+    )
   )
 )
 
