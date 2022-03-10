69a0f4796cb8c2e29e00450385a11f692f577b23
Coralieee <rhuang28@uw.edu>
2022-03-10 06:20:58 -0800

    fininhsed plot 3 and added theme for the app

--- p3_server.R
+++ p3_server.R
@@ -4,14 +4,19 @@ library(dplyr)
 library(stringr)
 library(readr)
 
+# Plot 1
 county_cases <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 wa <- county_cases %>% filter(state == "Washington")
 
-#plot2
-criminal_data_type<- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/criminal_data_type.csv?token=GHSAT0AAAAAABQW2MUKKXLPSEX4VDP3TLRMYRSWGNQ")
+# Plot 2
+criminal_data_type <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/criminal_data_type.csv?token=GHSAT0AAAAAABQW2MUKKXLPSEX4VDP3TLRMYRSWGNQ")
 personal_crime <- criminal_data_type[c(1:14, 37:50), ]
 property_crime <- criminal_data_type[c(16:27, 52:63), ]
 society_crime <- criminal_data_type[c(29:35, 65:71), ]
+
+# Plot 3
+king_county_total_crimes <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/crimes_in_King_Country_by_month%20copy.csv?token=GHSAT0AAAAAABQIMKIZSAV6DI3FTMEHASNSYRTDIMQ")
+
 
 server <- function(input, output) {
   
@@ -21,14 +26,16 @@ server <- function(input, output) {
       filter(county %in% input$user_category)
     
     Plot1 <- ggplot(data = wa) + 
-      geom_line(mapping = aes(x = date, y = cases, color= county))
+      geom_line(mapping = aes(x = date, y = cases, color = county))
     
     plotly_Plot1 <- ggplotly(Plot1)
     
     return(plotly_Plot1)
+    
   })
 
-  output$Plot2 <- renderPlotly({ 
+  output$Plot2 <- renderPlotly({
+    
     if (input$plot_type == 1) {
       ggplot(personal_crime) + 
         geom_col(aes(x=year, y=Incidents, fill = Offense))+
@@ -45,9 +52,35 @@ server <- function(input, output) {
         labs(title = "Society Crime Incidences", subtitle = "2019-2020",x = "Year", y = "Incidents") 
     }
     
-    output$Plot3 <- renderPlotly({
-      
-    })
+  })
+  
+  output$Plot3 <- renderPlotly({
+    
+    king_county_total_crimes <- king_county_total_crimes %>%
+      filter(Year %in% input$Year)
+    
+    plot3 <- ggplot(data = king_county_total_crimes) +
+      geom_line(mapping = aes(x = Month, y = Total_Crimes, color = Year)) +
+      geom_point(mapping = aes(x = Month, y = Total_Crimes, color = Year))
+      labs(title = "Total Crimes in King County from 2018 to 2020 by Month",
+           x = "Month", y = "Numbers of Crimes",
+           color = "Year")
+    
+    plotly_plot3 <- ggplotly(plot3) %>%
+      style(hoverlabel = list(bgcolor = "white"))
+    
+    return(plotly_plot3)
+    
+  })
+  
+  output$Plot3Summary <- renderText({
+    return("I found that crimes in Washington state has mostly occurred in King County in the recent years, thus 
+           I decided use King County as typical case to analyze. The reason I chose to plot these line chart and 
+           scatter plot was line chart can clearly show the trend that total number of crimes change during the 
+           year and scatter plot can intuitively show the difference in the total number of crimes in each month 
+           of different years. It can be seen from the chart that the total number of crimes in King County in 2020
+           does less than that in 2018 and 2019 to some extent, thus we can infer that Covid-19 does have an impact 
+           on the total number of crimes in King County.")
   })
 }
 
--- p3_ui.R
+++ p3_ui.R
@@ -2,19 +2,27 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
+introdction <- tabPanel(
+  "Introduction",
+  fluidPage(theme = bs_theme(primary = "#9370DB", font_scale = NULL, bootswatch = "cerulean"),
+            h4("Major Questions"),
+            p("...")
+  )
+)
 
 
+# Plot 1
 plot_sidebar1 <- sidebarPanel(
   selectInput(
     inputId = "user_category",
-    label = h4("Select County"),
+    label = h5("Select County: "),
     choices = wa$county,
     selected = "King",
     multiple = TRUE)
   
 )
 
-plot_main <- mainPanel(
+plot_main1 <- mainPanel(
   plotlyOutput(outputId = "Plot1" ),
   p("By looking at this chart, we can get a glance about trend of covid cases among counties.")
 )
@@ -23,44 +31,98 @@ plot_1 <- tabPanel(
   "Plot 1",
   sidebarLayout(
     plot_sidebar1,
-    plot_main
+    plot_main1
   )
 )
 
 
-
+# Plot 2
 plot_2 <- tabPanel(
   "Plot 2", 
     sidebarPanel(
       radioButtons(
         inputId = "plot_type",
-        label = h3("Different Distribution of Crimes"),
+        label = h5("Different Distribution of Crimes"),
         choices = list("Personal Crime" = 1, "Property Crime" = 2, "Society Crime" = 3), 
         selected = 1),
       ),
     mainPanel(
-      plotlyOutput("Plot2", width= "65%", height = "650px"),
-      p("This chart displays the number of incidents per Crime Type. Switch between categories to see the distribution from 2019-2020.")
+      plotlyOutput("Plot2", width = "65%", height = "650px"),
+      p("This chart displays the number of incidents per Crime Type. Switch between categories to see the distribution from 2019 to 2020.")
     )
 )
 
+
+# Plot 3
+plot_sidebar3 <- sidebarPanel(
+  selectInput(
+    inputId = "Year",
+    label = h5("Select Year: "),
+    choices = king_county_total_crimes$Year,
+    selected = "2020",
+    multiple = TRUE),
+)
+
+plot_main3 <- mainPanel(
+  tabsetPanel(
+    tabPanel("Plot", plotlyOutput(outputId = "Plot3"),   
+    p("This chart displays the total number of crimes in King County. 
+    Select years to compare the data from 2018 to 2020.")),
+    tabPanel("Summary", textOutput(outputId = "Plot3Summary"))
+  )
+)
+
+plot_3 <- tabPanel(
+  "Plot 3",
+  sidebarLayout(
+    plot_sidebar3,
+    plot_main3
+  )
+)
+
+
 conclusion <- tabPanel(
   "Conclusion",
-  h2("Takeaways"),
+  h4("Takeaways"),
   p("The three major takeaways are about covid cases, the crime number, and crime types."), 
-  p("The trend of the number of covid cases in each county in Washington State has been shown in the first plot. It can be clearly seen that covid-19 has been in Washington state since around February 2020, and there has been a linear increase in cases since then. By January next year, there has been an exponential increase in cases. It can be inferred that the delta variant, which is more infectious, started entering Washington state and became a major contributor to covid cases. The rate of increase slowed down in early February because more and more people are vaccinated and good anti-epidemic measures by local government. Among all the counties, King County has the highest number of covid cases due to its high county population. "),
-  h2("Insights"),
-  p("Generally speaking, there is a light positive correlation between covid cases and crime numbers, though this correlation may be caused by the differences in total number of populations."),
-  p("There are some regions which do not show this positive correlation: they have low covid cases but relatively high crime numbers, or have high crime numbers but with relatively low covid cases. Other social factors exist to influence these two variables."),
+  p("The trend of the number of covid cases in each county in Washington State has been shown in 
+     the first plot. It can be clearly seen that covid-19 has been in Washington state since around 
+     February 2020, and there has been a linear increase in cases since then. By January next year, 
+     there has been an exponential increase in cases. It can be inferred that the delta variant, 
+     which is more infectious, started entering Washington state and became a major contributor to 
+     covid cases. The rate of increase slowed down in early February because more and more people are 
+     vaccinated and good anti-epidemic measures by local government. Among all the counties, King 
+     County has the highest number of covid cases due to its high county population."),
+                    
+  h4("Insights"),
+  p("Generally speaking, there is a light positive correlation between covid cases and crime numbers, 
+     though this correlation may be caused by the differences in total number of populations."),
+  p("There are some regions which do not show this positive correlation: they have low covid cases 
+     but relatively high crime numbers, or have high crime numbers but with relatively low covid 
+     cases. Other social factors exist to influence these two variables."),
   p("The correlation between crime and covid still needs to be further explored."),
-  h2("Implications"),
-  p("The seriousness of the pandemic can be served as the degree to measure the effectiveness of the public health system. By looking at our graphs in covid cases distribution, administrators in public health system can have a more general idea of the condition about the covid spread as well as the security measures. Also, crime rates could be served as a standard to test the management of government. We are expecting to find a result of a clear correlation between crime rates and covid cases. To be more specific, we are expecting to find a positive relationship between crime rates and covid cases, which means covid facilities the crimes. In this way, in order to decrease crime rate in a region, policymakers can start with controlling the spread of covid. They can control the crime rate as well as the spread of covid, and maybe cooperate with the public health system.")
+                      
+  h4("Implications"),
+  p("The seriousness of the pandemic can be served as the degree to measure the effectiveness of the 
+     public health system. By looking at our graphs in covid cases distribution, administrators in 
+     public health system can have a more general idea of the condition about the covid spread as 
+     well as the security measures. Also, crime rates could be served as a standard to test the 
+     management of government. We are expecting to find a result of a clear correlation between crime 
+     rates and covid cases. To be more specific, we are expecting to find a positive relationship 
+     between crime rates and covid cases, which means covid facilities the crimes. In this way, in 
+     order to decrease crime rate in a region, policymakers can start with controlling the spread of 
+     covid. They can control the crime rate as well as the spread of covid, and maybe cooperate with 
+     the public health system.")
+
 )
+
 
 ui <- navbarPage(
   "P03",
+  introdction,
   plot_1,
   plot_2,
+  plot_3,
   conclusion
 )
 
