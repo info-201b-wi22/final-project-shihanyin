3d29406a3eb5dd06932c8cb71afd88452afa0f57
megcheu <megancheung@MacBook-Pro-4.local>
2022-03-09 19:32:00 -0800

    started plot2

--- p3_server.R
+++ p3_server.R
@@ -1,9 +1,14 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+library(stringr)
+library(readr)
 
-county_cases <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+county_cases <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
 wa <- county_cases %>% filter(state == "Washington")
+
+criminal_data_type_copy <- read_csv("criminal_data_type copy.csv")
+personal_crime <- criminal_data_type_copy[c(1:14, 37:50), ]
 
 server <- function(input, output) {
   
