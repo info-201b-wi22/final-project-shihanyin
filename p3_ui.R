e619c0635b2a05592a2dc816d2cb819e6246d11f
shihanyin <shihay3@uw.edu>
2022-03-10 12:51:07 -0800

    finished

--- p3_ui.R
+++ p3_ui.R
@@ -2,6 +2,19 @@ library(ggplot2)
 library(plotly)
 library(bslib)
 
+# Plot 1
+county_cases <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+wa <- county_cases %>% filter(state == "Washington") %>% summarise(date, county, cases)
+
+# Plot 2
+criminal_data_type <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/criminal_data_type.csv?token=GHSAT0AAAAAABQW2MUKKXLPSEX4VDP3TLRMYRSWGNQ")
+personal_crime <- criminal_data_type[c(1:14, 37:50), ]
+property_crime <- criminal_data_type[c(16:27, 52:63), ]
+society_crime <- criminal_data_type[c(29:35, 65:71), ]
+
+# Plot 3
+king_county_total_crimes <- read.csv("https://raw.githubusercontent.com/info-201b-wi22/final-project-shihanyin/main/crimes_in_King_Country_by_month%20copy.csv?token=GHSAT0AAAAAABQIMKIZSAV6DI3FTMEHASNSYRTDIMQ")
+
 introdction <- tabPanel(
   "Introduction",
   fluidPage(theme = bs_theme(primary = "#9370DB", font_scale = NULL, bootswatch = "cerulean"),
