3e7ebc729106048fab4ceadf5b39682dc3b1105f
cathy0103 <yyao8@uw.edu>
2022-03-09 23:11:37 -0800

    add a conclusion page and finished the insights and implications

--- p3_ui.R
+++ p3_ui.R
@@ -46,9 +46,22 @@ plot_2 <- tabPanel(
   )
 )
 
+conclusion <- tabPanel(
+  "Conclusion",
+  h2("Takeaways"),
+  p("xxxxx"),
+  h2("Insights"),
+  p("Generally speaking, there is a light positive correlation between covid cases and crime numbers, though this correlation may be caused by the differences in total number of populations."),
+  p("There are some regions which do not show this positive correlation: they have low covid cases but relatively high crime numbers, or have high crime numbers but with relatively low covid cases. Other social factors exist to influence these two variables."),
+  p("The correlation between crime and covid still needs to be further explored."),
+  h2("Implications"),
+  p("The seriousness of the pandemic can be served as the degree to measure the effectiveness of the public health system. By looking at our graphs in covid cases distribution, administrators in public health system can have a more general idea of the condition about the covid spread as well as the security measures. Also, crime rates could be served as a standard to test the management of government. We are expecting to find a result of a clear correlation between crime rates and covid cases. To be more specific, we are expecting to find a positive relationship between crime rates and covid cases, which means covid facilities the crimes. In this way, in order to decrease crime rate in a region, policymakers can start with controlling the spread of covid. They can control the crime rate as well as the spread of covid, and maybe cooperate with the public health system.")
+)
+
 ui <- navbarPage(
   "P03",
   plot_1,
-  plot_2
+  plot_2,
+  conclusion
 )
 
